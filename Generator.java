0cb83fcaf3c0a8e918b002a648bbafa3ad9b59a2
Cryolyst <doollist@gmail.com>
2024-05-14 00:32:50 -0700

    Ctrl + shift + f

--- Generator.java
+++ Generator.java
@@ -6,11 +6,10 @@ public class Generator {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		
+
 		System.out.println("How many words do you want for your password: ");
 		int pass_length = input.nextInt();
-		
-		
+
 		System.out.println(generatePassword.generate(pass_length));
 
 		input.close();
--- generatePassword.java
+++ generatePassword.java
@@ -8,23 +8,23 @@ public class generatePassword {
 		StringBuilder chars = new StringBuilder();
 		StringBuilder Password = new StringBuilder();
 		Random random = new Random();
-		
+
 		String lowercase = "abcdefghijklmnopqrstuvwxyz";
 		String uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 		String numbers = "0123456789";
 		String specialchars = "!@#$%^&*()_-+={}[]|.,<>;:";
-		
+
 		chars.append(lowercase);
 		chars.append(uppercase);
 		chars.append(numbers);
 		chars.append(specialchars);
-		
-		for (int i = 0 ; i <length; i++) {
+
+		for (int i = 0; i < length; i++) {
 			Password.append(chars.charAt(random.nextInt(chars.length())));
 		}
-		
+
 		return Password.toString();
-		
+
 	}
 
 }
