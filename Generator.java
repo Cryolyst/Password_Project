01bd0b8c4599bc16042f58f91c3d57b15d467437
Cryolyst <doollist@gmail.com>
2024-05-14 00:31:16 -0700

    Finished the code but need more refining

--- Generator.java
+++ Generator.java
@@ -7,10 +7,13 @@ public class Generator {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		
-		System.out.println("");
+		System.out.println("How many words do you want for your password: ");
 		int pass_length = input.nextInt();
 		
+		
+		System.out.println(generatePassword.generate(pass_length));
 
+		input.close();
 	}
 
 }
--- generatePassword.java
+++ generatePassword.java
@@ -1,10 +1,30 @@
 package Password_Project;
 
+import java.util.*;
+
 public class generatePassword {
 
 	public static String generate(int length) {
-		return null;
-
+		StringBuilder chars = new StringBuilder();
+		StringBuilder Password = new StringBuilder();
+		Random random = new Random();
+		
+		String lowercase = "abcdefghijklmnopqrstuvwxyz";
+		String uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		String numbers = "0123456789";
+		String specialchars = "!@#$%^&*()_-+={}[]|.,<>;:";
+		
+		chars.append(lowercase);
+		chars.append(uppercase);
+		chars.append(numbers);
+		chars.append(specialchars);
+		
+		for (int i = 0 ; i <length; i++) {
+			Password.append(chars.charAt(random.nextInt(chars.length())));
+		}
+		
+		return Password.toString();
+		
 	}
 
 }
