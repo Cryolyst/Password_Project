335296d3c508c2af2fc8562c9c55cc379a72b905
Cryolyst <doollist@gmail.com>
2024-05-13 21:22:19 -0700

    The beginning of the project

---
+++ Generator.java
@@ -0,0 +1,16 @@
+package Password_Project;
+
+import java.util.*;
+
+public class Generator {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("");
+		int pass_length = input.nextInt();
+		
+
+	}
+
+}
---
+++ generatePassword.java
@@ -0,0 +1,10 @@
+package Password_Project;
+
+public class generatePassword {
+
+	public String generatePassword(int length) {
+		return null;
+
+	}
+
+}
